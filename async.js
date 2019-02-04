e35eb634da517bcdbca0570d6603c296825f521f
GSarpong4423 <sarpongg@gmail.com>
2019-02-04 04:49:01 +0000

    promises

--- async.js
+++ async.js
@@ -1,32 +1,27 @@
 window.onload = function(){
     
-    var http = new xmlHttpRequest();
-    
-    http.onreadystatechange = function(){
-        if(http.readyState == 4 && http.status == 200){
-            console.log(JSON.parse(http.response));
-            
-        }
-        console.log(http);
-    };
-    
-    http.open("GET", "Ajax/tweets.json", true);
-    http.send();
-    
-    
-    // jquery method - same as above
-    $.get("Ajax/tweets.json", function(data){
-        console.log(data);
-    });
-    console.log("test");
-    
+    function get(url){
+        return new Promise(function(resolve, reject){
+            var xhttp = new XMLHttpRequest();
+            xhttp.onload = function(){
+                if (xhttp.status == 200){
+                    resolve(JSON.parse(xhttp.response));
+                } else{
+                    reject(xhttp.statusText);
+                }
+            };
+            xhttp.onerror = function(){
+                reject(xhttp.statusText);
+            };
+            xhttp.send();
+        });
+        
+    }
+    var promise = get("Ajax/tweets.json");
+    promise.then(function(tweets){
+        console.log(tweets);
+    }).catch(function(error){
+        console.log(error);
+    })
 };
-/* READY STATES
-
-0 - request not installed 
-1 - request has been set up
-2 - request has been set up
-3 - request is in process
-4 - request is complete
-
-*/
+    
